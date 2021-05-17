14c6eb797f3e50bc293809e3fba6094bd362d228
ujjvalksharma <ujjvalksharma@gmail.com>
2021-05-17 00:43:27 -0400

    project creation

---
+++ src/test/java/com/ujs/SampleDockerJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
